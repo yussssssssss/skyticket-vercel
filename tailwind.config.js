278512c13cf2e0639bfcc2c431fc5b4d21a3d9e9
yustyy <yusuf.acmaci@hotmail.com>
2025-12-10 17:14:55 +0300

    artlab added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tüm .js ve .jsx dosyalarını dahil ediyoruz
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
